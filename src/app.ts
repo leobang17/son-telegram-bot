1a47f514df8b07a561bf132a62841f7ac7c2ac8c
LeoBang <bluke8489@gmail.com>
2022-01-09 16:43:45 +0900

    webhook init function

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Response, Request } from "express";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 import axios from "axios";
 
-import bot from "./bot";
+import { webHookInit } from "./webhook";
 
 // envs
 dotenv.config();
@@ -12,7 +11,7 @@ const TELEGRAM_API: string = `https://api.telegram.org/bot${BOT_TOKEN}`;
 
 const URI: string = `/webhook/${BOT_TOKEN}`;
 const WEBHOOK_URL: string = NGROK_URL + URI;
-
+const chatIds: number[] = [5074918684, 1082663030];
 
 // express
 const app = express();
@@ -23,32 +22,50 @@ app.use(express.json());
 app.use(express.urlencoded());
 // app.use(bot.webhookCallback());
 
-
-// create webhook
-const init = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
-    console.log(res.data);
-};
-
-
-// startBot();
+// Routes.
 app.get("/", (req: Request, res: Response): void => {
     res.send("Hi There !");
+});
+
+app.get("/sendText", async (req: Request, res: Response): Promise<void> => {
+    chatIds.map(async (chatId: number, index: number) => {
+        await axios.post(`${TELEGRAM_API}/sendMessage`, {
+            chat_id: chatId,
+            text: "Hello world!"
+        });
+    });
+    // const res2 = await axios.post(`${TELEGRAM_API}/getChatMember`, {
+    //     chat_id: 5074918684,
+
+    // });
+    // console.log(res2.data);
+
+    res.send();
 });
 
 app.post(URI, async (req: Request, res: Response) => {
     console.log(req.body);
     const chatId: number = req.body.message.chat.id;
     const text: string = req.body.message.text;
+    const time: Date = new Date(req.body.message.date * 1000);
+    const photo = req.body.message.photo;
 
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id: chatId,
-        text: text
-    });
+    console.log(`Time: ${time}`);
+
+    // await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    //     chat_id: chatId,
+    //     text: text
+    // });
+
     return res.send();
-})
+});
+
+app.get('/getFootball', async (req: Request, res:Response) => {
+    
+});
+
 
 app.listen(PORT, async () => {
     console.log(`Server app listening at port: ${PORT}`);
-    await init();
+    await webHookInit();
 })

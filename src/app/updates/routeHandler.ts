5d88e0e14cbbe4f88845339bdd83d207c65a178f
LeoBang <bluke8489@gmail.com>
2022-01-19 00:24:04 +0900

    command handler

--- src/app/updates/routeHandler.ts
+++ src/app/updates/routeHandler.ts
@@ -1,8 +1,10 @@
+import { Commands } from "./interfaces/interfaces";
 import { EntityType, Message } from "./interfaces/telegramInterfaces";
+import { deleteUserId, insertUserId } from "./updateController";
 
 export const messageHandler = async (params: any) => {
     const { update_id, message }: { update_id: number, message: Message} = params;
-    
+
     // command 
     if (message.entities) {
         entityHandler(message);
@@ -23,15 +25,16 @@ export const memberStatusHandler = async <T>(params: T) => {
 }
 
 
-const entityHandler = (message: Message) => {
+const entityHandler = (message: Message): void => {
     const content: string = message.text as string;
     const parsedList: string[] | null = entityParser(content);
-
+    const chatId: number = message.from!.id;
+    
     for (let entity of message.entities!) {
-        console.log(entity);
         switch (entity.type) {
             case EntityType.BOT_COMMAND: 
-                commandHandler(message);
+                const command: string = parsedList.shift() as string;
+                commandHandler({command, chatId});
                 break;
         }
     }
@@ -39,11 +42,12 @@ const entityHandler = (message: Message) => {
 
 const entityParser = (content: string): string[] => {
     const contentList: string[] = content.split(" ");
-    const regex: RegExp = /^\/[a-z0-9]+$/i;
+    const commandRegex: RegExp = /^\/[a-z0-9]+$/i;
     const parsedList: string[] = new Array();
 
     for (let contentIter of contentList) {
-        const regexRes: RegExpExecArray | null = regex.exec(contentIter);
+        const regexRes: RegExpExecArray | null = commandRegex.exec(contentIter);
+
         if (regexRes) {
             parsedList.push(regexRes[0]);
         }
@@ -52,8 +56,16 @@ const entityParser = (content: string): string[] => {
     return parsedList;
 };
 
-const commandHandler = (message: Message) => {
-    const content: string = message.text as string;
+
+const commandHandler = async (params: { command: string, chatId: number})  => {
+    const { command, chatId } = params;
+    switch (command) {
+        case Commands.Start:
+            await insertUserId(chatId);
+            break;
     
-    
+        case Commands.Exit:
+            await deleteUserId(chatId);
+            break;
+    }
 }

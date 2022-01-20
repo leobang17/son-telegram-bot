0bd4ab8a3ece95a4003670d028f6c378f6fa57b4
LeoBang <bluke8489@gmail.com>
2022-01-21 00:55:31 +0900

    milli sec constants

--- src/app/timer/CustomTimeout.ts
+++ src/app/timer/CustomTimeout.ts
@@ -19,4 +19,3 @@ export default class CustomTimeout {
         return this.timeoutId;
     }
 }
-
---
+++ src/app/timer/constants.ts
@@ -0,0 +1,7 @@
+export enum MilliSec {
+    MINUTE = 60000,
+    HOUR = 3600000,
+    DAY = 86400000,
+    WEEK = 604800016.56,
+    MONTH = 2629800000,
+}

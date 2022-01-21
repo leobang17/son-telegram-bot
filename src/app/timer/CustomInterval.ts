c9bf85931c60fe4af08c1ea983a583b4dae2fa59
LeoBang <bluke8489@gmail.com>
2022-01-21 15:53:27 +0900

    timer with clear

--- src/app/timer/CustomInterval.ts
+++ src/app/timer/CustomInterval.ts
@@ -1,18 +1,36 @@
 export default class CustomInterval {
-    private intervalId: NodeJS.Timer;
-    
-    constructor (interval: number, executionFunc: { (): void}) {
-        this.intervalId = setInterval(
-            executionFunc, 
-            interval
-        );
+    private intervalId: NodeJS.Timer | null;
+    private executionFunc: { (): number | void };
+    private interval: number;
+
+    constructor (executionFunc: { (): void}, interval: number) {
+        this.executionFunc = executionFunc;
+        this.interval = interval
+        this.intervalId = null;
     }
 
-    kill() {
+    private async setCondition () {
+        const res = await this.executionFunc();
+        if (!res) {
+            this.kill();
+        }
+    }
+
+    public set() {
+        this.intervalId = setInterval(() => this.setCondition(), this.interval);
+        console.log(`interval started... \tper ${this.interval} millisec`);
+    }
+
+    public kill() {
+        if (!this.intervalId) {
+            console.log(`You should first use set() method before you use kill() method`);
+            return;
+        }
         clearInterval(this.intervalId);
+        console.log(`interval killed.`);
     }
 
-    getIntervalId() {
+    public getIntervalId() {
         return this.intervalId;
     }
 }
